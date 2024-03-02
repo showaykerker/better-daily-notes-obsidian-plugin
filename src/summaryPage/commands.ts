a03ddd3c52757275af82f21451bbe475fbaa0217
Hsu Hsiu Wei <df405102@gmail.com>
2024-03-03 03:57:35 +0800

    refactor: create summaryPage/ with commands and eventListerners

---
+++ src/summaryPage/commands.ts
@@ -0,0 +1,72 @@
+import { App, Notice, TFile } from 'obsidian';
+import { getDailyNotePath } from '../utils';
+import { BetterDailyNotesSettings } from '../settings/settings';
+import { openOrSwitchToNote } from '../commands';
+
+export async function CreateSummaryPageCommands(plugin: any) {
+    plugin.addCommand({
+        id: 'open-summary-page',
+        name: 'Open and update summary page',
+        callback: async () => {
+            await updateSummaryPage(plugin.app, plugin.settings, true, true);
+        }
+    })
+}
+
+export async function CreateSummaryPageRibbonIcons(plugin: any) {
+    plugin.addRibbonIcon(
+        'list',
+        'Open and update summary page',
+        async (evt: MouseEvent) => {
+            await updateSummaryPage(plugin.app, plugin.settings, true, true);
+        }
+    ).addClass('better-daily-notes-ribbon-class');
+}
+
+export async function updateSummaryPage(
+        app: App,
+        settings: BetterDailyNotesSettings,
+        createIfNotExists: boolean,
+        open: boolean): Promise<void> {
+
+    if (!settings.enableSummaryPage) {
+        new Notice("Summary Page is disabled. Not opening the summary page.");
+        return;
+    }
+    const summaryPagePath = settings.rootDir + '/' + settings.summaryPageFile + '.md';
+    const previousDailyNotePaths = getPreviousDailyNotePaths(app, settings, settings.summaryOfDaysCount);
+    // add links to the summary page
+    let summary = "";
+    for (let path of previousDailyNotePaths) {
+        summary += `![[${path}]]\n\n`;
+    }
+    const summaryPageExists = await app.vault.adapter.exists(summaryPagePath);
+    if (summaryPageExists) {
+        const summaryPage = app.vault.getAbstractFileByPath(summaryPagePath);
+        await app.vault.modify(summaryPage as TFile, summary);
+        if (open) { openOrSwitchToNote(app, summaryPagePath); }
+        return;
+    }
+
+    if (createIfNotExists){
+        await app.vault.create(summaryPagePath, summary);
+    }
+
+    if (open) { openOrSwitchToNote(app, summaryPagePath); }
+}
+
+function getPreviousDailyNotePaths(app: App, settings: BetterDailyNotesSettings, days: number): string[] {
+    const rootDir = settings.rootDir;
+    const dateFormat = settings.dateFormat;
+    let date = new Date();
+    const paths: string[] = [];
+    for (let i = 1; i <= days; i++) {
+        const targetNotePath = getDailyNotePath(
+            rootDir, 0, dateFormat, date);
+        if (app.vault.getAbstractFileByPath(targetNotePath)) {
+            paths.push(targetNotePath);
+        }
+        date.setDate(date.getDate() - 1);
+    }
+    return paths;
+}
---
+++ src/summaryPage/eventListeners.ts
@@ -0,0 +1,42 @@
+import BetterDailyNotePlugin from '../main';
+import { checkValidDailyNotePath } from '../utils';
+import { TAbstractFile, TFile } from 'obsidian';
+import { updateSummaryPage } from './commands';
+
+export function CreateSummaryPageEventListener(plugin: BetterDailyNotePlugin) {
+    plugin.registerEvent(
+        plugin.app.vault.on(
+            "delete",
+            async (file: TAbstractFile) => {
+                if (!plugin.settings.enableSummaryPage) return;
+                if (!(file instanceof TFile)) return;
+                if (!checkValidDailyNotePath(file.path, plugin.settings.dateFormat)) return;
+                await updateSummaryPage(plugin.app, plugin.settings, false, false);
+            }
+        )
+    );
+    plugin.registerEvent(
+        plugin.app.vault.on(
+            "rename",
+            async (file: TAbstractFile, oldPath: string) => {
+                if (!plugin.settings.enableSummaryPage) return;
+                if (!(file instanceof TFile)) return;
+                if (!checkValidDailyNotePath(oldPath, plugin.settings.dateFormat) &&
+                    !checkValidDailyNotePath(file.path, plugin.settings.dateFormat)) return;
+                await updateSummaryPage(plugin.app, plugin.settings, false, false);
+            }
+        )
+    );
+    plugin.registerEvent(
+        plugin.app.vault.on(
+            "create",
+            async (file: TAbstractFile) => {
+                if (!plugin.settings.enableSummaryPage) return;
+                if (!(file instanceof TFile)) return;
+                if (!checkValidDailyNotePath(file.path, plugin.settings.dateFormat)) return;
+                await updateSummaryPage(plugin.app, plugin.settings, false, false);
+            }
+        )
+    );
+}
+
