1fde422fd58e90323f37d7ee78e2d90cddcb3f5e
showay <df405102@gmail.com>
2024-03-27 22:47:18 +0800

    refactor[compatibility]: isolate main functionality

--- src/compatibility/eventListeners.ts
+++ src/compatibility/eventListeners.ts
@@ -1,9 +1,6 @@
 import { Notice, TFile } from 'obsidian';
 import BetterDailyNotePlugin from '../main';
-import dayjs from 'dayjs';
-import { checkValidDailyNote, getDailyNotePath } from '../utils';
-import { createDirsIfNotExists } from 'src/dailyNotes/fileSystem';
-import { getSettingsFromExternalPlugin } from './utils';
+import { getSettingsFromExternalPlugin, moveDailyNote } from './utils';
 
 async function checkExternalPlugins(plugin: BetterDailyNotePlugin): Promise<string[]> {
     if (plugin.settings.compatibleDateFormats.length === 0) return [];
@@ -24,40 +21,12 @@ export async function createCompatibilityEventListener(plugin: BetterDailyNotePl
             plugin.app.vault.on(
                 "create",
                 async (file: TFile) => {
-                    if (!(file.extension === "md")) return;
-                    const createdByThisPlugin = checkValidDailyNote(file, plugin.settings);
-
-                    // Check if filename is valid date format
-                    let fileBasenameDate = null;
-                    for (const format of plugin.settings.compatibleDateFormats) {
-                        fileBasenameDate = dayjs(file.basename, format, true);
-                        if (fileBasenameDate.isValid()) break;
-                    }
-
-                    if (createdByThisPlugin) return;
-                    if (fileBasenameDate?.isValid()) {
-                        const dailyNotePath = getDailyNotePath(plugin.settings, fileBasenameDate.toDate(), false);
-                        if (plugin.app.vault.getAbstractFileByPath(dailyNotePath) == null) {
-                            new Notice(`Daily note ${file.name} created by external plugin, will be renamed to `+
-                                dailyNotePath + " in 1 second.", 2500);
-                            const dailyNoteDir = dailyNotePath.substring(0, dailyNotePath.lastIndexOf("/"));
-                            createDirsIfNotExists(plugin.app, dailyNoteDir);
-                            // add template
-                            const templateFile = plugin.app.vault.getAbstractFileByPath(plugin.settings.templateFile);
-                            if (templateFile instanceof TFile) {
-                                let template = await plugin.app.vault.read(templateFile);
-                                await plugin.app.vault.modify(file, template);
-                                new Notice(`Template "${plugin.settings.templateFile}" applied to ${dailyNotePath}`);
-                            }
-                            await new Promise((resolve) => setTimeout(resolve, 1000));
-                            await plugin.app.vault.rename(file, dailyNotePath);
-                            new Notice("Daily note renamed to " + dailyNotePath, 2500);
-                        }
-                        else {
-                            new Notice("Daily note created by external plugin, " +
-                                `but a daily note with the same name ${dailyNotePath} already exists.` +
-                                "No action will be taken.", 2500);
-                        }
+                    let moveResult = await moveDailyNote(plugin.app, plugin.settings, file, true, false, false);
+                    if (moveResult.startsWith("already exists")){
+                        const dailyNotePath = moveResult.split(": ")[1];
+                        new Notice("Daily note created by external plugin, " +
+                            `but a daily note with the same name "${dailyNotePath}" already exists.` +
+                            "No action will be taken.", 2500);
                     }
                 }
             )
--- src/compatibility/utils.ts
+++ src/compatibility/utils.ts
@@ -1,5 +1,8 @@
-import { App, Notice } from 'obsidian';
+import { App, Notice, TFile } from 'obsidian';
 import { BetterDailyNotesSettings } from 'src/settings/settings';
+import dayjs from 'dayjs';
+import { checkValidDailyNote, getDailyNotePath } from '../utils';
+import { createDirsIfNotExists } from 'src/dailyNotes/fileSystem';
 
 export async function getSettingsFromExternalPlugin(app: App, settings: BetterDailyNotesSettings): Promise<Set<string>> {
     let formats: Set<string> = new Set<string>()
@@ -24,4 +27,60 @@ async function getDateFormat(
     const format = keys.reduce((acc, key) => acc[key], externalSettingsDict) || settings.dateFormat;
     console.log(`Better Daily Notes: ${externalPluginName} plugin uses date format: ${format}`);
     return format;
+}
+
+export async function moveDailyNote(
+        app: App,
+        settings: BetterDailyNotesSettings,
+        file_: string | TFile,
+        shouldWait: boolean,
+        noTemplate: boolean,
+        copyInstead: boolean): Promise<string> {
+    let file = file_ instanceof TFile ? file_ : app.metadataCache.getFirstLinkpathDest(file_, "");
+    if (file == null) return "null";
+    if (!(file.extension === "md")) return "not .md file";
+    const createdByThisPlugin = checkValidDailyNote(file, settings);
+
+    // Check if filename is valid date format
+    let fileBasenameDate = null;
+    for (const format of settings.compatibleDateFormats) {
+        fileBasenameDate = dayjs(file.basename, format, true);
+        if (fileBasenameDate.isValid()) break;
+    }
+
+    if (createdByThisPlugin) return "created by this plugin";
+    if (fileBasenameDate?.isValid()) {
+        const dailyNotePath = getDailyNotePath(settings, fileBasenameDate.toDate(), false);
+        if (app.vault.getAbstractFileByPath(dailyNotePath) == null) {
+            if (shouldWait){
+                new Notice(`Daily note ${file.name} created by external plugin, will be renamed to `+
+                    dailyNotePath + " in 1 second.", 2500);
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+            }
+            const dailyNoteDir = dailyNotePath.substring(0, dailyNotePath.lastIndexOf("/"));
+            createDirsIfNotExists(app, dailyNoteDir);
+            if (!noTemplate) {
+                // add template
+                const templateFile = app.vault.getAbstractFileByPath(settings.templateFile);
+                if (templateFile instanceof TFile) {
+                    let template = await app.vault.read(templateFile);
+                    await app.vault.modify(file, template);
+                    new Notice(`Template "${settings.templateFile}" applied to ${dailyNotePath}`);
+                }
+            }
+            if (copyInstead) {
+                await app.vault.copy(file, dailyNotePath);
+                new Notice("Daily note copied to " + dailyNotePath, 2500);
+            }
+            else {
+                await app.vault.rename(file, dailyNotePath);
+                new Notice("Daily note renamed to " + dailyNotePath, 2500);
+            }
+            return dailyNotePath;
+        }
+        else {
+            return `already exists: ${dailyNotePath}`;
+        }
+    }
+    return "not daily note";
 }
